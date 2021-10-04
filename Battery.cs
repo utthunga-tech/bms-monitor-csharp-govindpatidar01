b4203f98cda6567170ea20560eae0c825e106376
Govind Patidar <govindpatidar01@gmail.com>
2021-10-05 01:15:00 +0530

    Added class Battery

---
+++ Battery.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace BMS
+{
+    internal class Battery
+    {
+        private float _temperature;
+        private float _soc;
+        private float _chargeRate;
+
+        internal float Temperature
+        {
+            get => _temperature; 
+            set => _temperature = value;
+        }
+        internal float Soc
+        {
+            get => _soc;
+            set => _soc = value;
+        }
+
+        internal float ChargeRate
+        {
+            get => _chargeRate; 
+            set => _chargeRate = value; 
+        }
+
+        public Battery(float temp, float soc, float chargeRate)
+        {
+            _temperature =temp;
+            _soc = soc;
+            _chargeRate = chargeRate;
+        }
+
+        internal bool BatteryIsOk()
+        {
+            if (!IsTempInRange(_temperature))
+            {
+                Console.WriteLine("Temperature is out of range!");
+                return false;
+            }
+            else if (!IsSOCIsInRange(_soc))
+            {
+                Console.WriteLine("State of Charge is out of range!");
+                return false;
+            }
+            else if (!IsChargeRateIsInRange(_chargeRate))
+            {
+                Console.WriteLine("Charge Rate is out of range!");
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsTempInRange(float temperature)
+        {
+            if (temperature < 0 || temperature > 45)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsSOCIsInRange(float soc)
+        {
+            if (soc < 20 || soc > 80)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsChargeRateIsInRange(float chargeRate)
+        {
+            if (chargeRate > 0.8)
+            {
+                Console.WriteLine("Charge Rate is out of range!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- checker.cs
+++ checker.cs
@@ -1,37 +1,30 @@
 using System;
 using System.Diagnostics;
-
-class Checker
+using BMS;
+public class Checker
 {
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
-    }
-
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
+    static void ExpectTrue(bool expression) 
+    {
+        if(!expression) 
+        {
             Console.WriteLine("Expected true, but got false");
             Environment.Exit(1);
         }
     }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
+    static void ExpectFalse(bool expression)
+    {
+        if(expression)
+        {
             Console.WriteLine("Expected false, but got true");
             Environment.Exit(1);
         }
     }
-    static int Main() {
-        ExpectTrue(batteryIsOk(25, 70, 0.7f));
-        ExpectFalse(batteryIsOk(50, 85, 0.0f));
+    static int Main() 
+    {
+        Battery btr = new Battery(25, 70, 0.7f);
+        ExpectTrue(btr.BatteryIsOk());
+        Battery btr1 = new Battery(50, 85, 0.0f);
+        ExpectFalse(btr1.BatteryIsOk());
         Console.WriteLine("All ok");
         return 0;
     }
