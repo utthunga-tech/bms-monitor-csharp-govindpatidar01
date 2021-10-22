8661a066326ca1d0fd226e025c94fbb773a9419f
Govind Patidar <SESA617999@se.com>
2021-10-22 11:35:05 +0530

    Reverted code

--- checker.cs
+++ checker.cs
@@ -4,13 +4,18 @@ using System.Diagnostics;
 class Checker
 {
     static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
+        if(temperature < 0 || temperature > 45) 
+        {
             Console.WriteLine("Temperature is out of range!");
             return false;
-        } else if(soc < 20 || soc > 80) {
+        }
+        else if(soc < 20 || soc > 80) 
+        {
             Console.WriteLine("State of Charge is out of range!");
             return false;
-        } else if(chargeRate > 0.8) {
+        }
+        else if(chargeRate > 0.8)
+        {
             Console.WriteLine("Charge Rate is out of range!");
             return false;
         }
