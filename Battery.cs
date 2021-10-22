ceb9f7d49ededb00b0d491a38b8ce017602b7b33
Govind Patidar <SESA617999@se.com>
2021-10-22 11:26:00 +0530

    Revert "Added const"
    This reverts commit 4b6f03df5aa6e302ccc2c31c93dad2ffb8f4b5a3.

--- Battery.cs
+++ Battery.cs
@@ -10,57 +10,23 @@ namespace BMS
         private float _soc;
         private float _chargeRate;
 
-        private float _temperatureMin;
-        private float _socMin;
-
-        private float _temperatureMax;
-        private float _socMax;
-        private float _chargeRateMax;
-
         internal float Temperature
         {
             get => _temperature; 
             set => _temperature = value;
-        }
-
-        internal float TemperatureMin
-        {
-            get => _temperatureMin;
-            set => _temperatureMin = value;
-        }
-
-        internal float TemperatureMax
-        {
-            get => _temperatureMax;
-            set => _temperatureMax = value;
         }
         internal float Soc
         {
             get => _soc;
             set => _soc = value;
         }
-        internal float SocMax
-        {
-            get => _socMax;
-            set => _socMax = value;
-        }
-        internal float SocMin
-        {
-            get => _socMin;
-            set => _socMin = value;
-        }
-      
+
         internal float ChargeRate
         {
             get => _chargeRate; 
             set => _chargeRate = value; 
         }
-        
-        internal float ChargeRateMax
-        {
-            get => _chargeRateMax;
-            set => _chargeRateMax = value;
-        }
+
         public Battery(float temp, float soc, float chargeRate)
         {
             _temperature =temp;
@@ -90,7 +56,7 @@ namespace BMS
 
         private bool IsTempInRange(float temperature)
         {
-            if (temperature < _temperatureMin || temperature > _temperatureMax)
+            if (temperature < 0 || temperature > 45)
             {
                 return false;
             }
@@ -99,7 +65,7 @@ namespace BMS
 
         private bool IsSOCIsInRange(float soc)
         {
-            if (soc < _socMin || soc > _socMax)
+            if (soc < 20 || soc > 80)
             {
                 return false;
             }
@@ -108,7 +74,7 @@ namespace BMS
 
         private bool IsChargeRateIsInRange(float chargeRate)
         {
-            if (chargeRate > _chargeRateMax)
+            if (chargeRate > 0.8)
             {
                 Console.WriteLine("Charge Rate is out of range!");
                 return false;
